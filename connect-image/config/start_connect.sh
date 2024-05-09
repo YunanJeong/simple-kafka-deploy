16523617b1f19292b06a80fd0b48b22ae6db03f9
yunanjeong <njhgle@gmail.com>
2024-05-09 16:45:40 +0900

    comment

--- connect-image/config/start_connect.sh
+++ connect-image/config/start_connect.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# properties파일에 같은 key가 여러 번 나오면 가장 마지막 기술된 값이 사용됨
+# broker주소와 replication factor 설정 같은 필수설정 외 나머지 요소들은 CONNECT_EXTRA_CONFIG를 사용
+# CONNECT_EXTRA_CONFIG: connect-distributed.properties와 동일한 syntax로 여러 줄이 한 번에 들어간다.
+
 # 필수 커넥트 설정 반영
 echo "plugin.path=/opt/bitnami/kafka/plugins" >> /opt/bitnami/kafka/config/connect-distributed.properties
 echo "bootstrap.servers=$CONNECT_BOOTSTRAP_SERVERS" >> /opt/bitnami/kafka/config/connect-distributed.properties
